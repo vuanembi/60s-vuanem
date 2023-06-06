0d4f98b27425bf3a11530ab0e14eb109c15c85bd
HM <hieumdd@gmail.com>
2023-06-06 09:06:28 +0700

    misc: Clean up wizard.tsx

--- pages/wizard.tsx
+++ pages/wizard.tsx
@@ -9,15 +9,15 @@ import {
     useQuestion5,
     useQuestion6,
 } from '../hooks/use-questions';
+import { GetProductsFormValues } from '../hooks/use-wizard-form';
 import { WizardStep } from '../components/wizard/step';
 import { RadioCardText } from '../components/radio-group/radio-card-text';
 import { RadioCardImage } from '../components/radio-group/radio-card-image';
 import { WizardProgress } from '../components/wizard/progress';
 import { WizardResult } from '../components/wizard/result';
-import { GetProductsFormValues } from '../hooks/use-wizard-form';
 
 const Wizard = () => {
-    const { control, handleSubmit, reset, watch } = useForm<GetProductsFormValues>();
+    const { control, handleSubmit, watch } = useForm<GetProductsFormValues>();
 
     return (
         <form onSubmit={handleSubmit((data) => console.log(data))}>
