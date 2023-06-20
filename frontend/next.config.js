ca2c6ea2b430f8fba60e7332f2e2aff317bd6dfb
HM <hieumdd@gmail.com>
2023-06-20 14:57:14 +0700

    Revert "feat: Add svg options"
    This reverts commit fe546c03e3fcb4e212e67d0c01cb41f1abab75b4.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,9 +2,6 @@
 const nextConfig = {
     reactStrictMode: true,
     images: {
-        dangerouslyAllowSVG: true,
-        contentDispositionType: 'attachment',
-        contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
         remotePatterns: [
             {
                 protocol: 'https',
